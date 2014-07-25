1640be7fc73835eaaee4470a4cc200d5155a9b24
maxb <maxb.personal@gmail.com>
2014-07-24 23:00:54 -0700

    Fixing init.d script.

--- broker/scripts/tunneldigger-broker.init.d
+++ broker/scripts/tunneldigger-broker.init.d
@@ -26,7 +26,7 @@ PIDFILE=/var/run/$NAME.pid
 STARTSTOP_OPTS='--quiet --exec $DAEMON --chdir $DAEMON_CHDIR \
  --chuid $DAEMON_USER --user $DAEMON_USER --group $DAEMON_GROUP'
 START_OPTS='--background --make-pidfile'
-DAEMON_ARGS='l2tp_broker.py /opt/tunneldigger/tunneldigger/broker/l2tp_broker_implicator.cfg'
+DAEMON_ARGS='l2tp_broker.py /opt/tunneldigger/tunneldigger/broker/l2tp_broker.cfg'
 
 # Activate virtualenv Python environment
 . /opt/tunneldigger/broker/env_tunneldigger/bin/activate
