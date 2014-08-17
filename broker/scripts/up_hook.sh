6103c1c7ef7a2355ed30fa66b22a4897e9f55e4a
Max B <maxb.personal@gmail.com>
2014-08-17 20:06:44 +0000

    Fixing the up_hook script some.

--- broker/scripts/up_hook.sh
+++ broker/scripts/up_hook.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
+set -x
+set +e
 
 INTERFACE="$3"
 
-ifconfig $INTERFACE up
 batctl if add $INTERFACE
+ifconfig $INTERFACE up
 
+ifdown bat0
 ifup bat0
+
+iptables -t nat -D POSTROUTING ! --destination 10.0.0.0/8 -j MASQUERADE
+iptables -t nat -I POSTROUTING ! --destination 10.0.0.0/8 -j MASQUERADE
+
+echo "1" > /proc/sys/net/ipv4/ip_forward
+
