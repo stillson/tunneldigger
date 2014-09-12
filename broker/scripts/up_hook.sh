3fb64cd1805c215750371d3503147bd9e25cfd65
Maxb <maxb.personal@gmail.com>
2014-09-11 21:43:10 -0700

    Fixing masquerade rule in tunneldigger (hopefully).

--- broker/scripts/up_hook.sh
+++ broker/scripts/up_hook.sh
@@ -10,8 +10,8 @@ ifconfig $INTERFACE up
 ifdown bat0
 ifup bat0
 
-iptables -t nat -D POSTROUTING ! --destination 10.0.0.0/8 -j MASQUERADE
-iptables -t nat -I POSTROUTING ! --destination 10.0.0.0/8 -j MASQUERADE
+iptables -t nat -D POSTROUTING --source 10.0.0.0/8 ! --destination 10.0.0.0/8 -j MASQUERADE
+iptables -t nat -I POSTROUTING --source 10.0.0.0/8 ! --destination 10.0.0.0/8 -j MASQUERADE
 
 echo "1" > /proc/sys/net/ipv4/ip_forward
 
