89f2fb4df11cdbeee5017be0f251e638139976d1
Alex Papazoglou <papazoga@gmail.com>
2014-09-12 14:23:50 -0700

    Control tunnels via ifupdown.

--- broker/scripts/up_hook.sh
+++ broker/scripts/up_hook.sh
@@ -1,17 +1,2 @@
 #!/bin/sh
-set -x
-set +e
-
-INTERFACE="$3"
-
-batctl if add $INTERFACE
-ifconfig $INTERFACE up
-
-ifdown bat0
-ifup bat0
-
-iptables -t nat -D POSTROUTING --source 10.0.0.0/8 ! --destination 10.0.0.0/8 -j MASQUERADE
-iptables -t nat -I POSTROUTING --source 10.0.0.0/8 ! --destination 10.0.0.0/8 -j MASQUERADE
-
-echo "1" > /proc/sys/net/ipv4/ip_forward
-
+ifup $3=mesh-tunnel
